43bbb770c2b404e44b188b1c3b7228bb7ba276fb
IN57KC <torsten.wiederkehr@ing.com>
2025-12-02 08:20:03 +0100

    Implemented chopNumbersInHalf

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -9,6 +9,13 @@ class Day02 {
         }
         return allNumbers
     }
+
+    fun chopNumberInHalf(number: Int): Pair<String, String> {
+        val numberAsString = number.toString()
+        val firstHalf = numberAsString.substring(0, numberAsString.length / 2)
+        val secondHalf = numberAsString.substring(numberAsString.length / 2)
+        return Pair(firstHalf, secondHalf)
+    }
 }
 
 fun main() {
--- src/test/kotlin/Day02Test.kt
+++ src/test/kotlin/Day02Test.kt
@@ -6,7 +6,6 @@ class Day02Test {
 
     @Test
     fun create_all_numbers() {
-        // arrange
 
         // act
         val result = Day02().allNumbers("11-22")
@@ -15,6 +14,45 @@ class Day02Test {
         assertThat(result).containsExactly(
             11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22
         )
-        
     }
+
+    @Test
+    fun chop_number_in_half_1() {
+
+        // act
+        val result = Day02().chopNumberInHalf(1)
+
+        // assert
+        assertThat(result).isEqualTo(Pair("", "1"))
+    }
+
+    @Test
+    fun chop_number_in_half_11() {
+
+        // act
+        val result = Day02().chopNumberInHalf(11)
+
+        // assert
+        assertThat(result).isEqualTo(Pair("1", "1"))
+    }
+
+    @Test
+    fun chop_number_in_half_111() {
+
+        // act
+        val result = Day02().chopNumberInHalf(111)
+
+        // assert
+        assertThat(result).isEqualTo(Pair("1", "11"))
+    }
+
+    @Test
+    fun chop_number_in_half_1111() {
+        // act
+        val result = Day02().chopNumberInHalf(1111)
+
+        // assert
+        assertThat(result).isEqualTo(Pair("11", "11"))
+    }
+
 }
