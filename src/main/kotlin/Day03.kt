abf9738beca7b2a0eadbce3a765f3dd91a044614
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-12-03 19:41:20 +0100

    Solved day 3 part 1

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,2 +1,32 @@
 class Day03 {
+
+    fun readData(fileName: String): List<String> = Resources.resourceAsListOfString(fileName)
+
+    fun findHighestDigitInString(bank: String): Char {
+        return bank.maxOrNull() ?: throw IllegalArgumentException("Cannot find highest digit in empty string")
+    }
+
+    fun substringAfterHighestDigit(maxDigit: Char, bank: String): String {
+        val index = bank.indexOf(maxDigit)
+        return bank.substring(index + 1)
+    }
+
+    fun joltage(bank: String): Int {
+        val firstDigit = findHighestDigitInString(bank.dropLast(1))
+        val remainingBank = substringAfterHighestDigit(firstDigit, bank)
+        val secondDigit = findHighestDigitInString(remainingBank)
+        val joltage = "${firstDigit}${secondDigit}".toInt()
+        return joltage
+    }
+
+    fun totalJoltage(joltageList: List<String>): Int {
+        return joltageList.sumOf { Day03().joltage(it) }
+    }
+
+}
+
+fun main() {
+    val data = Day03().readData("Day03_InputData.txt")
+    val part1 = Day03().totalJoltage(data)
+    println("Part 1: $part1")
 }
--- src/test/kotlin/Day03Test.kt
+++ src/test/kotlin/Day03Test.kt
@@ -1,2 +1,93 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
 class Day03Test {
+
+    @Test
+    fun read_data() {
+        // arrange
+
+        // act
+        val data = Day03().readData("Day03_TestData.txt")
+
+        // assert
+        assertThat(data).containsExactly(
+            "987654321111111",
+            "811111111111119",
+            "234234234234278",
+            "818181911112111",
+        )
+    }
+
+    @Test
+    fun find_highest_digit_in_string() {
+
+        // act
+        val result = Day03().findHighestDigitInString("987654321111111")
+
+        // assert
+        assertThat(result).isEqualTo('9')
+    }
+
+    @Test
+    fun substringAfterHighestDigit() {
+
+        // act
+        val result = Day03().substringAfterHighestDigit('9', "987654321111111")
+
+        // assert
+        assertThat(result).isEqualTo("87654321111111")
+    }
+
+    @Test
+    fun joltage_987654321111111() {
+        // act
+        val joltage = Day03().joltage("987654321111111")
+
+        // assert
+        assertThat(joltage).isEqualTo(98)
+
+    }
+
+    @Test
+    fun joltage_811111111111119() {
+        // act
+        val joltage = Day03().joltage("811111111111119")
+
+        // assert
+        assertThat(joltage).isEqualTo(89)
+    }
+
+    @Test
+    fun joltage_234234234234278() {
+        // act
+        val joltage = Day03().joltage("234234234234278")
+
+        // assert
+        assertThat(joltage).isEqualTo(78)
+    }
+
+    @Test
+    fun joltage_818181911112111() {
+        // act
+        val joltage = Day03().joltage("818181911112111")
+
+        // assert
+        assertThat(joltage).isEqualTo(92)
+    }
+
+    @Test
+    fun solve_part1() {
+        // arrange
+        val data = Day03().readData("Day03_TestData.txt")
+
+        // act
+        val totalJoltage = Day03().totalJoltage(data)
+
+        // assert
+        assertThat(totalJoltage).isEqualTo(357)
+    }
+
 }
+
