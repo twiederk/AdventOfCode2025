c3407ef2987bbb41a4375a466893ba4271d21aa7
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-12-06 19:20:48 +0100

    Implemented read data.

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,2 +1,25 @@
+
 class Day05 {
+
+    fun readData(fileName: String): Pair<List<LongRange>, List<Long>> {
+        val lines = Resources.resourceAsListOfString(fileName)
+        val ranges = mutableListOf<LongRange>()
+        val numbers = mutableListOf<Long>()
+        var readingRanges = true
+        for (line in lines) {
+            if (line.isBlank()) {
+                readingRanges = false
+                continue
+            }
+            if (readingRanges) {
+                val parts = line.split("-")
+                val start = parts[0].toLong()
+                val end = parts[1].toLong()
+                ranges.add(LongRange(start, end))
+            } else {
+                numbers.add(line.toLong())
+            }
+        }
+        return Pair(ranges, numbers)
+    }
 }
--- src/test/kotlin/Day05Test.kt
+++ src/test/kotlin/Day05Test.kt
@@ -1,2 +1,17 @@
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
 class Day05Test {
+
+    @Test
+    fun read_data() {
+
+        // act
+        val data: Pair<List<LongRange>, List<Long>> = Day05().readData("Day05_TestData.txt")
+
+        // assert
+        assertThat(data.first.size).isEqualTo(4)
+        assertThat(data.second.size).isEqualTo(6)
+
+    }
 }
