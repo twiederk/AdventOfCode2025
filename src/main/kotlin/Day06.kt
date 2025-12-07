bcad0e13e889ab9a2a3a43696ba38bda454df9c7
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-12-07 18:19:31 +0100

    Implemented calculate column

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,2 +1,25 @@
-class Day06 {
+class Day06(
+    private val numbers: List<List<Int>>,
+    private val operators: List<Char>
+) {
+    fun calculateColumn(columnId: Int): Int {
+        when (operators[columnId]) {
+            '+' -> {
+                var sum = 0
+                for (i in numbers.indices) {
+                    sum += numbers[i][columnId]
+                }
+                return sum
+            }
+            '*' -> {
+                var product = 1
+                for (i in numbers.indices) {
+                    product *= numbers[i][columnId]
+                }
+                return product
+            }
+            else -> throw IllegalArgumentException("Unsupported operator: ${operators[columnId]}")
+        }
+    }
+
 }
--- src/test/kotlin/Day06Test.kt
+++ src/test/kotlin/Day06Test.kt
@@ -1,2 +1,50 @@
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
 class Day06Test {
+
+    private val numbers = listOf(
+        listOf(123,328,51,64),
+        listOf(45,64,387,23),
+        listOf(6,98,215,314),
+    )
+
+    private val operators = listOf('*','+','*','+')
+
+    @Test
+    fun calculate_column_0() {
+        // act
+        val result = Day06(numbers, operators).calculateColumn(0)
+
+        // assert
+        assertThat(result).isEqualTo(33210)
+    }
+
+    @Test
+    fun calculate_column_1() {
+        // act
+        val result = Day06(numbers, operators).calculateColumn(1)
+
+        // assert
+        assertThat(result).isEqualTo(490)
+    }
+
+    @Test
+    fun calculate_column_2() {
+        // act
+        val result = Day06(numbers, operators).calculateColumn(2)
+
+        // assert
+        assertThat(result).isEqualTo(4243455)
+    }
+
+    @Test
+    fun calculate_column_3() {
+        // act
+        val result = Day06(numbers, operators).calculateColumn(3)
+
+        // assert
+        assertThat(result).isEqualTo(401)
+    }
+
 }
