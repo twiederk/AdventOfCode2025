6ac3e8d3109093f110db27bda22cd41a4d0fdb04
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-12-08 05:17:28 +0100

    Implemented find starting position

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,2 +1,11 @@
 class Day07 {
+
+    fun readData(filename: String): List<String> {
+        return Resources.resourceAsListOfString(filename)
+    }
+
+    fun startingPosition(line: String): Int {
+        return line.indexOf('S')
+    }
+
 }
--- src/test/kotlin/Day07Test.kt
+++ src/test/kotlin/Day07Test.kt
@@ -1,2 +1,44 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class Day07Test {
+
+    @Test
+    fun read_data() {
+        // arrange
+
+        // act
+        val grid: List<String> = Day07().readData("Day07_TestData.txt")
+
+        // assert
+        assertThat(grid).containsExactly(
+            ".......S.......",
+            "...............",
+            ".......^.......",
+            "...............",
+            "......^.^......",
+            "...............",
+            ".....^.^.^.....",
+            "...............",
+            "....^.^...^....",
+            "...............",
+            "...^.^...^.^...",
+            "...............",
+            "..^...^.....^..",
+            "...............",
+            ".^.^.^.^.^...^.",
+            "...............",
+        )
+    }
+
+    @Test
+    fun starting_position() {
+        // arrange
+
+        // act
+        val startingPosition: Int = Day07().startingPosition(".......S.......")
+
+        // assert
+        assertThat(startingPosition).isEqualTo(7)
+    }
 }
