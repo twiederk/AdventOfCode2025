50d1b27ad43f916e7b28c80dc0d90cd765bcdff4
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-12-10 06:24:30 +0100

    Notes to day 10

--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -1,2 +1,10 @@
 class Day10Test {
+
+    // [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+    // start position [...]
+    // target position [.##.]
+    // next: (3) (1,3) (2) (2,3) (0,2) (0,1)
+    // distance to target: target position - next position
+
+
 }
