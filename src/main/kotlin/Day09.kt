7d981c71a3b688eeeba4a72ece377784e5f5838c
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-12-10 05:19:06 +0100

    Implemented read data

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,2 +1,11 @@
 class Day09 {
+
+    fun readData(string: String): List<Point2D> {
+        val rawData = Resources.resourceAsListOfString(string)
+        return rawData.map {
+            val parts = it.split(",")
+            Point2D(parts[0].toInt(), parts[1].toInt())
+        }
+    }
+
 }
--- src/test/kotlin/Day09Test.kt
+++ src/test/kotlin/Day09Test.kt
@@ -1,2 +1,24 @@
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
 class Day09Test {
+
+    @Test
+    fun read_data() {
+        // act
+        val result: List<Point2D> = Day09().readData("Day09_TestData.txt")
+
+        // assert
+        Assertions.assertThat(result).containsExactly(
+            Point2D(7, 1),
+            Point2D(11, 1),
+            Point2D(11, 7),
+            Point2D(9, 7),
+            Point2D(9, 5),
+            Point2D(2, 5),
+            Point2D(2, 3),
+            Point2D(7, 3)
+        )
+    }
+
 }
