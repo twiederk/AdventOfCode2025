1b8c04ca629ca63afa84b9453460a02a61c50b2c
IN57KC <torsten.wiederkehr@ing.com>
2025-12-10 12:56:34 +0100

    First implementation ofDijkstra

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -8,15 +8,36 @@ class Day10 {
     // next: (3) (1,3) (2) (2,3) (0,2) (0,1)
     // distance to target: target position - next position
 
-    fun dijkstra(goal: String, buttons: List<List<Int>>): Int {
+    fun dijkstra(goal: BitSet, buttons: List<List<Int>>): Int {
         val seen = mutableSetOf<EngineState>()
         val queue = PriorityQueue<Work>()
-        return 0
+
+        queue += Work(EngineState(BitSet(goal.size()), 0), goal.size())
+
+        while (queue.isNotEmpty()) {
+            val work = queue.poll()
+            if (work.engineState.lights == goal) return work.engineState.steps
+
+            work.engineState.nextStates(buttons)
+                .filterNot { engineState -> engineState in seen }
+                .forEach { engineState ->
+                    queue += Work(engineState, work.engineState.steps + 1)
+                    seen += engineState
+                }
+        }
+        throw IllegalStateException("No route to goal")
     }
 }
 
-class Work {
-
+data class Work(
+    val engineState: EngineState,
+    val distance: Int
+) : Comparable<Work> {
+    override fun compareTo(other: Work): Int {
+        val xor: BitSet = engineState.lights.clone() as BitSet
+        xor.xor(other.engineState.lights)
+        return xor.cardinality()
+    }
 }
 
 data class EngineState(
@@ -29,5 +50,9 @@ data class EngineState(
             newBitset.flip(index)
         }
         return EngineState(newBitset, steps + 1)
+    }
+
+    fun nextStates(buttons: List<List<Int>>): List<EngineState> {
+        return buttons.map { next(it) }
     }
 }
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -1,19 +1,8 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.util.BitSet
-import kotlin.test.Ignore
 
 class Day10Test {
-
-    @Test
-    @Ignore
-    fun solve_first_line() {
-        // act
-        val result = Day10().dijkstra(".##.", listOf(listOf(1, 3), listOf(2, 3), listOf(0, 2), listOf(0, 1)))
-
-        // assert
-        assertThat(result).isEqualTo(2)
-    }
 
     @Test
     fun next_state() {
@@ -24,4 +13,56 @@ class Day10Test {
         val expectedEngineState = EngineState(BitSet(4).apply { set(1); set(3) }, 1)
         assertThat(engineState).isEqualTo(expectedEngineState)
     }
+
+    @Test
+    fun next_states() {
+        // arrange
+        val buttons = listOf(
+            listOf(1, 3),
+            listOf(2, 3),
+            listOf(0, 2),
+            listOf(0, 1)
+        )
+
+        // act
+        val engineState = EngineState(BitSet(4), 0).nextStates(buttons)
+
+        // assert
+        val expectedEngineStates = listOf(
+            EngineState(BitSet(4).apply { set(1); set(3) }, 1),
+            EngineState(BitSet(4).apply { set(2); set(3) }, 1),
+            EngineState(BitSet(4).apply { set(0); set(2) }, 1),
+            EngineState(BitSet(4).apply { set(0); set(1) }, 1)
+        )
+        assertThat(engineState).isEqualTo(expectedEngineStates)
+    }
+
+    @Test
+    fun solve_line_1() {
+        // [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1)
+
+        // arrange
+        val goal = BitSet(4).apply { set(1); set(2) }
+
+        // act
+        val result = Day10().dijkstra(goal, listOf(listOf(1, 3), listOf(2, 3), listOf(0, 2), listOf(0, 1)))
+
+        // assert
+        assertThat(result).isEqualTo(2)
+    }
+
+    @Test
+    fun solve_line_2() {
+        // [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4)
+
+        // arrange
+        val goal = BitSet(5).apply { set(3) }
+
+        // act
+        val result = Day10().dijkstra(goal, listOf(listOf(0,2,3,4), listOf(2,3), listOf (0,4), listOf (0,1,2), listOf (1,2,3,4)))
+
+        // assert
+        assertThat(result).isEqualTo(3)
+    }
+
 }
