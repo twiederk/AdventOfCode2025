4573fbd38f9e1c1e4b0d086823c5bfb65eb81f50
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2025-12-14 14:02:48 +0100

    Implement part 1

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,2 +1,24 @@
-class Day12 {
+class Day12(
+    val shapes: List<Int>
+) {
+
+    fun fit(region: Region): Int {
+        var requiredSpace = 0
+        for (i in shapes.indices) {
+            requiredSpace += shapes[i] * region.quantity[i]
+        }
+        if (region.size >= requiredSpace) {
+            return 1
+        }
+        return 0
+    }
+
+    fun part1(regions: List<Region>): Int {
+        return regions.sumOf { fit(it) }
+    }
 }
+
+class Region(
+    val size: Int,
+    val quantity: List<Int>
+)
--- src/test/kotlin/Day12Test.kt
+++ src/test/kotlin/Day12Test.kt
@@ -1,2 +1,66 @@
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
 class Day12Test {
+
+    private val shapes = listOf(7, 7, 7, 7, 7, 7)
+
+    @Test
+    fun example_1() {
+        // arrange
+        // 4x4: 0 0 0 0 2 0
+        val region = Region(16, listOf(0, 0, 0, 0, 2, 0))
+
+        // act
+        val result = Day12(shapes).fit(region)
+
+        // assert
+        assertThat(result).isEqualTo(1)
+    }
+
+    @Test
+    fun example_2() {
+        // arrange
+        //12x5: 1 0 1 0 2 2
+        val region = Region(60, listOf(1, 0, 1, 0, 2, 2))
+
+        // act
+        val result = Day12(shapes).fit(region)
+
+        // assert
+        assertThat(result).isEqualTo(1)
+    }
+
+    @Test
+    fun example_3() {
+        // arrange
+        //12x5: 1 0 1 0 3 2
+
+        val region = Region(60, listOf(1, 0, 1, 0, 3, 2))
+
+        // act
+        val result = Day12(shapes).fit(region)
+
+        // assert
+        assertThat(result).isEqualTo(1)
+    }
+
+    @Test
+    fun solve_part1() {
+        // arrange
+        val regions = listOf(
+            Region(16, listOf(0, 0, 0, 0, 2, 0)),
+            Region(60, listOf(1, 0, 1, 0, 2, 2)),
+            Region(60, listOf(1, 0, 1, 0, 3, 2))
+        )
+
+        // act
+        val result = Day12(shapes).part1(regions)
+
+        // assert
+        assertThat(result).isEqualTo(3)
+
+
+    }
 }
+
