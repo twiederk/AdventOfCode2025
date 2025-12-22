5f80462f81a574213d0532e17dbe63885b2e0b5e
IN57KC <torsten.wiederkehr@ing.com>
2025-12-22 16:57:08 +0100

    Added first test for day 7 part 2

--- src/test/kotlin/Day07Test.kt
+++ src/test/kotlin/Day07Test.kt
@@ -105,13 +105,50 @@ class Day07Test {
         assertThat(result).isEqualTo(21)
     }
 
+//    000000000011111
+//    012345678901234
+//    .......S.......
+//    .......1.......  (7,1)
+//    ......|^|......
+//    ......1.1......  (6,1) (8,1)
+//    .....|^|^|.....
+//    .....1.2.1.....  (5,1) (7,2) (9,1)
+//    ....|^|^|^|....
+//    ....1.3.3.1....  (4,1) (6,3) (8,3) (10,1)
+//    ....^.^...^....
+//    ...1.4.331.1...  (3,1) (5,4) (7,3) (8,3) (9,1) (11,1)
+//    ...^.^...^.^...
+//    ..1.5.434.2.1..  (2,1) (4,5) (6,4) (7,3) (8,4) (10,2) (12,1)
+//    ..^...^.....^..
+//    .1.154.74.21.1.  (1,1) (3,1) (4,5) (5,4) (7,7) (8,4) (10,2) (11,1) (13,1)
+//    .^.^.^.^.^...^.
+//    1.2.1.1.1.211.1  (0,1) (2,2) (4,10) (6,11) (8,11) (10,2) (11,1) (12,1) (14,1)
+//        0 1 1
+//
+//    1+2+10+11+11+2+1+1+1=40
+
     @Test
-    fun solve_part2() {
+    fun downwardTimeline_row_2() {
+        // arrange
+        val beams = mapOf(7 to 1L)
+
         // act
-        val result: Int = Day07().part2(grid)
+        val result = Day07().downwardTimeline(grid, beams, 2)
 
         // assert
-        assertThat(result).isEqualTo(40)
+        assertThat(result).hasSize(2)
+        assertThat(result[6]).isEqualTo(1)
+        assertThat(result[8]).isEqualTo(1)
+    }
+
+
+    @Test
+    fun solve_part2() {
+//        // act
+//        val result: Int = Day07().part2(grid)
+//
+//        // assert
+//        assertThat(result).isEqualTo(40)
     }
 
 }
